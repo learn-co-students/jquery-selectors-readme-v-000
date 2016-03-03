cdbb1102038109fc9a97ec3906606acfb77de6b0
Justin Thomas <jthomas0209@gmail.com>
2016-03-03 12:04:09 -0500

    Done

--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,23 @@
 'use-strict';
 
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem( ){
+  return $('#pic-list li:first-child');
+}
+
 // declare your functions here...
