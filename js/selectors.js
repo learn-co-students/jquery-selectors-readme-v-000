4b25b2cf17969eacbc6329971b81c64c54b9a7c5
Vannak Chhay <vannak.chhay1@gmail.com>
2016-05-10 19:26:38 -0400

    done

--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,25 @@
 'use-strict';
 
 // declare your functions here...
+function paragraphSelector(){
+  var paragraph = $('p');
+  return paragraph;
+}
+
+function lastImageSelector(){
+  var lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector(){
+  var babyNinja = $('#baby-ninja');
+  return babyNinja;
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem(){
+  return $('ul#pic-list li:first-child');
+}
