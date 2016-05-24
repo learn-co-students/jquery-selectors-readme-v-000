269f890cf6e9216afd57e223c5560c3cb3f67f2c
Fox Mic <michelfuchs0@gmail.com>
2016-05-24 15:21:28 +1000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,23 @@
 'use-strict';
 
 // declare your functions here...
+
+function paragraphSelector () {
+  paragraph = $('p'); return paragraph;
+}
+
+function lastImageSelector () {
+  image = $('img:last'); return image;
+}
+
+function ninjaBabySelector () {
+  ninja = $('#baby-ninja'); return ninja;
+}
+
+function divSelector () {
+  div = $('.pics'); return div;
+}
+
+function firstListItem () {
+  list = $('#pic-list li:first-child'); return list;
+}
