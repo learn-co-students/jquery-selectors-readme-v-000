38f1925a8d5f988cae514e497aecb88c5708cd54
Llubomir Stanojlovic <llustanojlovic@gmail.com>
2016-05-31 15:13:34 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
 // declare your functions here...
+//select all p tags (paragraph)
+function paragraphSelector(){
+  return $('p');
+}
+//return last image on the page
+function lastImageSelector(){
+  return $('img:last');
+}
+
+//select the img id using #, to get the baby
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+//select last 2 divs. Use " . " to select classes
+function divSelector(){
+  return $('.pics');
+}
+
+//return the first child in unordered list of pic-list
+function firstListItem(){
+  return $('ul#pic-list li:first');
+}
