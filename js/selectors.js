34118929edc8ebd54e7badae5cb7779294c77a3b
James Novak <jamesnovak90@gmail.com>
2016-06-10 21:12:26 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('div:last ul li:last-child img');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child')
+}
+
