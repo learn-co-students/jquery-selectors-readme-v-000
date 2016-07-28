20bac81641f7879bdc4530316e8fd3d8f415a2d2
Jason Casares <jasonc@csdrafting.com>
2016-07-28 19:57:50 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,12 @@
 // declare your functions here...
+function paragraphSelector() {
+    $("p").html();
+}
+
+function lastImageSelector() {
+  $("div img:last");
+}
+
+function ninjaBabySelector() {
+  $("img[alt='baby-ninja']")
+}
