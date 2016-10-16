f3302b9882c2bf9c62168bd19ff6d00f650f4366
DEREJE GEMECHU <dereje88@gmail.com>
2016-10-16 04:31:35 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img').last();s
+
+}
+
+
+function ninjaBabySelector() {
+  return $('img').first();
+}
+
+function divSelector() {
+  return $('div.pics');
+}
+
+function firstListItem() {
+  return $('ul li').first();
+}
