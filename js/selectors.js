4be395264d17db540c65bae609454704c4e07968
DownsideupmediaUSA <brandondecarlo@live.com>
2016-12-05 12:54:57 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
 // declare your functions here...
+function paragraphSelector(){
+  // returns a paragraph
+  return $('p');
+}
+
+function lastImageSelector(){
+  // returns last image
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  // returns child selector
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem(){
+  return $('ul#pic-list li:first-child');
+}
