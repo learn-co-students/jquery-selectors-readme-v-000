bfd709a1c6ec7e5603d9f562c4d259d4318f48cb
Joe <jsbenning4@gmail.com>
2017-01-21 20:08:03 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  var paragraphs = $('p');
+  return paragraphs;
+}
+
+function lastImageSelector() {
+  var lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector() {
+  var ninja = $('img#baby-ninja');
+  return ninja;
+}
+
+function divSelector() {
+  var pics = $('.pics');
+  return pics;
+}
+
+function firstListItem() {
+  var firstItem = $('ul#pic-list li:first');
+  return firstItem; 
+}
