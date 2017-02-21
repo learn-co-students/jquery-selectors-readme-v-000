5bb0fe7a805ae3f38ef22493249f8138d0a52fcf
johnram528 <johnram528@gmail.com>
2017-02-21 11:44:48 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('div img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul li:first')
+}
+
+  // 5) Selectors firstListItem() should return the first list item in the unordered list:
+  //    ReferenceError: firstListItem is not defined
+  //     at Context.<anonymous> (test/index-test.js:23:12)
+
