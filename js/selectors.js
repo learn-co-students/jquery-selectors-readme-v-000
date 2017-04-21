338a108326a349fff24bdc77dc326f7a1f4219b8
Holger Paas <holger.paas@gmail.com>
2017-04-21 02:44:53 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function firstListItem(){
+  return $('ul#pic-list li:first-child')
+}
+
+function divSelector(){
+  return $('.pics')
+}
+
+function lastImageSelector(){
+  return $('.pics img:last')
+}
