b6ea1fe2d866112ad242a8602c1482054337011a
Amanda George <github email address>
2017-08-10 14:36:37 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  var paragraphs = $('p')
+  return paragraphs
+}
+
+function lastImageSelector() {
+  var image = $('img:last')
+  return image
+}
+
+function ninjaBabySelector() {
+  var image = $('#baby-ninja')
+  return image
+}
+
+function divSelector() {
+  var div = $('.pics')
+  return div
+}
+
+function firstListItem() {
+  var item = $('ul li:first')
+  return item
+}
