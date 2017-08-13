26836cabe055592b1b08239732135b3fc8b41cdd
debjohnson33 <debjohnson_11@yahoo.com>
2017-08-12 21:54:40 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -11,9 +11,9 @@ function ninjaBabySelector() {
 }
 
 function divSelector() {
-	return $('div.pics')
+	return $('.pics')
 }
 
 function firstListItem() {
-	return $('ul li:first')
+	return $('ul#pic-list li:first-child')
 }
