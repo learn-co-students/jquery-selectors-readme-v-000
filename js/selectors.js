1de88bf2b733e52c51b3e06bf68b92028b6b9bfd
Sean <sean.r.quillen@gmail.com>
2017-08-30 16:05:12 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function divSelector() {
+	return $('.pics')
+}	
+
+function ninjaBabySelector() {
+	return $('#baby-ninja')	
+}	
+
+function paragraphSelector() {
+	return $('p')
+}
+
+function lastImageSelector() {
+	return $('div.pics img:last')
+}
+
+function firstListItem() {
+	return $('ul li:first')
+}
